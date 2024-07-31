710581af8dcd2b963c9d847b1ae56614fb5b7408
Phạm Tuấn Anh <tuananhtybg.99@gmail.com>
2024-07-31 10:31:10 +0700

    up

--- js/atlantica.js
+++ js/atlantica.js
@@ -1,51 +1,51 @@
-$(document).ready(function(){
-						   
-	// Dropdown Script	   
-	function mainmenu(){
-	$(" #nav ul ").css({display: "none"}); // Opera Fix
-	$(" #nav li").hover(function(){
-			$(this).find('ul:first').css({visibility: "visible",display: "none"}).show(400);
-			},function(){
-			$(this).find('ul:first').css({visibility: "hidden"});
-			});
+$(document).ready(function() {
+	// Dropdown Script
+	function mainmenu() {
+		$("#nav ul").css({ display: "none" }); // Opera Fix
+		$("#nav li").hover(
+			function() {
+				$(this).find('ul:first').css({ visibility: "visible", display: "none" }).show(400);
+			},
+			function() {
+				$(this).find('ul:first').css({ visibility: "hidden" });
+			}
+		);
 	}
-	
-	 $(document).ready(function(){					
-		mainmenu();
-	});
- 
+
+	mainmenu();
+
 	// Activate PrettyPhoto Lightbox handle
 	$("a[rel^='gallery']").prettyPhoto();
-	
+
 	// PNG Fix
-	$(document).pngFix(); 	
-	
+	$(document).pngFix();
+
 	// Activate jCarousel
-	function mycarousel_initCallback(carousel)
-	{
-    	// Disable autoscrolling if the user clicks the prev or next button.
-    	carousel.buttonNext.bind('click', function() {
-    	    carousel.startAuto(0);
-    	});
-		
-    	carousel.buttonPrev.bind('click', function() {
-    	    carousel.startAuto(0);
-    	});
-		
-    	// Pause autoscrolling if the user moves with the cursor over the clip.
-    	carousel.clip.hover(function() {
-    	    carousel.stopAuto();
-    	}, function() {
-    	    carousel.startAuto();
-    	});
-	};
+	function mycarousel_initCallback(carousel) {
+		// Disable autoscrolling if the user clicks the prev or next button.
+		carousel.buttonNext.bind('click', function() {
+			carousel.startAuto(0);
+		});
 
+		carousel.buttonPrev.bind('click', function() {
+			carousel.startAuto(0);
+		});
 
-    	jQuery('#mycarousel').jcarousel({
-    	    auto: 6,
-    	    wrap: 'last',
-    	    animation: 'slow',
-    	    initCallback: mycarousel_initCallback
-    });
+		// Pause autoscrolling if the user moves with the cursor over the clip.
+		carousel.clip.hover(
+			function() {
+				carousel.stopAuto();
+			},
+			function() {
+				carousel.startAuto();
+			}
+		);
+	}
 
+	$('#mycarousel').jcarousel({
+		auto: 6,
+		wrap: 'last',
+		animation: 'slow',
+		initCallback: mycarousel_initCallback
+	});
 });
